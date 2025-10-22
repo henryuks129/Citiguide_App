089b14a7ea076b87fcca147bd42c282c112cfc09
Eduvie Etieyibo <eduvieetieyibo91@gmail.com>
2025-10-22 18:24:18 +0100

    favorites,profile and city overview page done

---
+++ lib/profile/pages/change_password.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChangePasswiord extends StatelessWidget {
+  const ChangePasswiord({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
